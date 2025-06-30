08d47aeb83b2cc2ec8cbd026233812e1e5664417
tanish <tanish12@gmail.com>
2025-06-30 12:02:40 +0530

    adding new file

---
+++ First.java
@@ -0,0 +1,12 @@
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Tanish Nag");
+
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+
+        System.out.println(c);
+    }
+}
