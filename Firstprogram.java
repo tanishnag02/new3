aeb33acee06e9046bd95b1dc44c5e183938296b9
tanish <tanish12@gmail.com>
2025-07-02 12:46:29 +0530

    Notepad work

---
+++ Firstprogram.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Firstprogram
+{
+public static void main(String[] args)
+ {
+  System.out.println("Tanish Nag");
+ Scanner sc = new Scanner(System.in);
+ }
+}
